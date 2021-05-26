546ac44adfb5087d4419f2fb8148b69f9b135434
dramati <jatinsaini191@gmail.com>
2021-05-26 19:56:22 +0530

    titles changes + icons added

--- js/dropdown-menu.js
+++ js/dropdown-menu.js
@@ -1,5 +1,5 @@
 if(window.innerWidth < 600){
-    function droppingdown(){
+    function droppingdon(){
         var x = document.getElementById("k21droppeddown");
         if (x.style.display == "none"){
             x.style.display="block";
